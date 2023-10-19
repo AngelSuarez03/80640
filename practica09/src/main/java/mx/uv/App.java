3a6eb162c4833d801720b263ab24e6d0c8d34502
AngelSuarez03 <Angnusa11@gmail.com>
2023-10-19 12:55:06 -0600

    practica09

--- practica09/src/main/java/mx/uv/App.java
+++ practica09/src/main/java/mx/uv/App.java
@@ -1,5 +1,7 @@
 package mx.uv;
 
+import static spark.Spark.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hola Mundo!" );
+        get("/",
+            (rq, rs) -> "<h1>Hola Mundo!</h1>");
+        get("/ruta1",
+            (rq, rs) -> "<h1>Adios Mundo!</h1>");
+        get("/ruta2",
+            (rq, rs) -> "<h1>Que tal Mundo!</h1>");
+        get("/ruta3",
+            (rq, rs) -> "{'alumno':'jhon','matricula':'s001','carrera':'tc'}");
     }
 }
